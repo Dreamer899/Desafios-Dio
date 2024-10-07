63cf40f465c9740eeff1c7251a774c6421c06d3d
Dreamer899 <je626883@gmail.com>
2024-10-07 12:27:49 -0300

    Create classicador_de_herois.py

---
+++ Logica_Programação/classicador_de_herois.py
@@ -0,0 +1,38 @@
+
+
+while True:
+  heroi = input("Digite o nome do Heroi:")
+  exp_heroi = int(input("digite a exp do heroi:"))
+  if(exp_heroi <= 1000):
+    print("Heroi:"+ heroi)
+    print("Classicaçaõ:Ferro")
+  elif(exp_heroi >= 1000 and  exp_heroi < 2001):
+    print("Heroi:"+ heroi)
+    print("Classicaçaõ:Bronze")
+  elif(exp_heroi > 2000 and  exp_heroi < 5001):
+    print("Heroi:"+ heroi)
+    print("Classicaçaõ:Prata")
+  elif(exp_heroi > 5000 and  exp_heroi < 7001):
+    print("Heroi:"+ heroi)
+    print("Classicaçaõ:Ouro")
+  elif(exp_heroi > 7000 and  exp_heroi < 8001):
+    print("Heroi:"+ heroi)
+    print("Classicaçaõ:Platina")
+  elif(exp_heroi > 8000 and  exp_heroi < 9001):
+    print("Heroi:"+ heroi)
+    print("Classicaçaõ:Ascendente")
+  elif(exp_heroi > 9000 and  exp_heroi < 10001):
+    print("Heroi:"+ heroi)
+    print("Classicaçaõ:Imortal")
+  elif(exp_heroi >= 10001):
+    print("Heroi:"+ heroi)
+    print("Classicaçaõ:Radiante")
+  
+  
+
+
+
+
+
+
+
